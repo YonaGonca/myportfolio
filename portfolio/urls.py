5ca9d0fab970d2ddb7345a2eee6fb69f9e728ed5
YonaGonca <ygoncalves5a@gmail.com>
2025-02-24 16:21:28 +0000

    -improve SEO adding meta labels -improve alternatives texts to images and aria labels -changing some images format to webp to reduce de size -change the aboutme url from /me/ to /about-me/ -improve some styles in aboutme.css

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('projects/', views.projects, name='projects'),
-    path('me/', views.aboutme, name='aboutme'),
+    path('about-me/', views.aboutme, name='aboutme'),
     path('change-language/<str:lang_code>/', views.change_language, name='change_language'),
     path("send-email/", views.contact_view, name="send_email"),
 ]
